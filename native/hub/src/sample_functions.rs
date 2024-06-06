492b817938c1fef8c61a53e8751e2fc10e2f5def
jaydipnasit-oceanmtech <145960611+jaydipnasit-oceanmtech@users.noreply.github.com>
2024-06-06 17:09:44 +0530

    .
    .

--- native/hub/src/sample_functions.rs
+++ native/hub/src/sample_functions.rs
@@ -4,7 +4,6 @@ use rinf::debug_print;
 pub async fn tell_treasure() {
     debug_print!("*-*-*-*-*-* started");
 
-    let mut current_value: i32 = 1;
     let mut receiver = OverlayInputData::get_dart_signal_receiver(); // GENERATED
     while let Some(dart_signal) = receiver.recv().await {
         let my_precious_data = dart_signal.message;
@@ -13,6 +12,5 @@ pub async fn tell_treasure() {
             output_image: my_precious_data.overlay_image,
         }
         .send_signal_to_dart();
-        current_value += 1;
     }
 }
